4043c7975ef4aa798d6e2cc4f3cfb519fa517495
sea0416 <sea0416@naver.com>
2022-10-03 12:42:46 -0500

    Tools/Node update
    Console log the request url and set the response header

--- Tools/Node/index.js
+++ Tools/Node/index.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const url = require('url');
+const crypto = require('crypto');
 
 const app = express();
 
@@ -6,7 +8,11 @@ app.listen(3000, () => { console.log(`Example app listening on port 3000`)})
 const currentDate = new Date();
 const timestamp = currentDate.getTime();
 app.get('/tasks',(req, res) => {
-    console.log('http://localhost:3000/tasks This is the url');
+    var rand = crypto.randomUUID(); //uuid
+    var url_parts = req.url; //
+    console.log(`The current url is http://localhost:3000${url_parts}`);
     console.log(timestamp + 'This is the time');
+    console.log(`The xrequest-id of header is ${rand}`)
+    res.setHeader('xrequest-id', `${rand}`);
     res.send('Hello World!')})
 app.get('/index',(req, res) => {res.send('Hello index!')})
